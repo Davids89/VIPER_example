19f7dafba759b1039a70ac14a10f0a5873475c48
David Luque <daviluqui@gmail.com>
2017-07-12 12:13:32 +0200

    Routing magic

--- Routing.swift
+++ Routing.swift
@@ -6,4 +6,21 @@
 //  Copyright © 2017 david. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Routing {
+    
+    let viewController: TableViewController = TableViewController()
+    let presenter: Presenter = Presenter()
+    let interactor: Interactor = Interactor()
+    var navigationController: UINavigationController?
+    
+    init() {
+        viewController.presenter = presenter
+        presenter.view = viewController
+        presenter.interactor = interactor
+        presenter.routing = self
+        interactor.presenter = presenter
+        navigationController = UINavigationController(rootViewController: viewController)
+    }
+}
