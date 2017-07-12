7a403bbe9fc26e2b1a28056a4ad4453b0798a73d
David Luque <daviluqui@gmail.com>
2017-07-12 12:12:56 +0200

    Presenter skeleton

--- Presenter.swift
+++ Presenter.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2017 david. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol PresenterProtocol {
+    func addNewObjects()
+}
+
+protocol InteractorResponse {
+    func updateObjects(objects: [String])
+}
+
+class Presenter: PresenterProtocol{
+
+    var view: TableViewController?
+    var interactor: Interactor?
+    var routing: Routing?
+    
+    //MARK: Protocol implementation
+    func addNewObjects() {
+        
+    }
+}
+
+extension Presenter: InteractorResponse{
+    func updateObjects(objects: [String]) {
+        
+    }
+}
